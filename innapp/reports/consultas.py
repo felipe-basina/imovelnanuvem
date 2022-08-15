af38c1562ea3dedc26644016d1e3a678cc0b8e4c
felipe-basina <felipe.basina@gmail.com>
2022-08-15 10:20:49 -0300

    Refatoracao das consultas para utilizar inner join quando necessario; adicionado consultas para recuperar o total a ser considerado para pagamento de IR PF.

--- innapp/reports/consultas.py
+++ innapp/reports/consultas.py
@@ -45,28 +45,48 @@ def recuperar_administracao_ano():
     return cursor.fetchall()
 
 
-def consulta_mes_ano(tabela, coluna_data, coluna_valor):
+def recuperar_ir_pf_mes_ano(ano):
+    cursor = connection.cursor()
+    consulta = consulta_mes_ano('aluguel_tbl', 'dt_recebimento', 'num_aluguel', inner_join=True)
+    cursor.execute(consulta, [ano])
+    return cursor.fetchall()
+
+
+def recuperar_ir_pf_ano():
+    cursor = connection.cursor()
+    consulta = consulta_ano('aluguel_tbl', 'dt_recebimento', 'num_aluguel', inner_join=True)
+    cursor.execute(consulta)
+    return cursor.fetchall()
+
+
+def consulta_mes_ano(tabela, coluna_data, coluna_valor, inner_join=False):
     return (
         'SELECT concat(lpad(cast(extract(month from _coluna_data_) as text), 2, \'0\'), \'/\', extract(year from _coluna_data_)), sum(_coluna_valor_) '
-        'FROM _tabela_ '
+        'FROM _tabela_ _inner_join_ '
         'WHERE _coluna_data_ <= now() '
         'AND extract(year from _coluna_data_) = %s '
         'group by concat(lpad(cast(extract(month from _coluna_data_) as text), 2, \'0\'), \'/\', extract(year from _coluna_data_)) '
         'order by concat(lpad(cast(extract(month from _coluna_data_) as text), 2, \'0\'), \'/\', extract(year from _coluna_data_)) desc '
+        .replace('_inner_join_', inquilino_inner_join_clause() if inner_join else '')
         .replace('_coluna_data_', coluna_data)
         .replace('_coluna_valor_', coluna_valor)
         .replace('_tabela_', tabela)
     )
 
 
-def consulta_ano(tabela, coluna_data, coluna_valor):
+def consulta_ano(tabela, coluna_data, coluna_valor, inner_join=False):
     return (
         'SELECT cast(extract(year from _coluna_data_) as bigint), sum(_coluna_valor_) '
-        'FROM _tabela_ '
+        'FROM _tabela_ _inner_join_ '
         'WHERE _coluna_data_ <= now() '
         'group by extract(year from _coluna_data_) '
         'order by extract(year from _coluna_data_) desc '
+        .replace('_inner_join_', inquilino_inner_join_clause() if inner_join else '')
         .replace('_coluna_data_', coluna_data)
         .replace('_coluna_valor_', coluna_valor)
         .replace('_tabela_', tabela)
     )
+
+
+def inquilino_inner_join_clause():
+    return ' INNER JOIN inquilino_tbl inq ON _tabela_.idt_inquilino = inq.idt_inquilino and inq.desc_tipo = \'PF\' '
