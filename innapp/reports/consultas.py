bbfa3633ad03cb8a293ef6845f10fcc830d6abdc
felipe-basina <felipe.basina@gmail.com>
2022-08-15 09:58:42 -0300

    Refatoracao para reutulizar estrutura base de consultas; adicionado consultas para gerar relatorio de administracao por mes e ano, ano.

--- innapp/reports/consultas.py
+++ innapp/reports/consultas.py
@@ -5,45 +5,68 @@ from django.db import connection
 
 def recuperar_aluguel_mes_ano(ano):
     cursor = connection.cursor()
-    consulta = ('SELECT concat(lpad(cast(extract(month from dt_recebimento) as text), 2, \'0\'), \'/\', extract(year from dt_recebimento)), sum(num_aluguel) '
-                'FROM aluguel_tbl '
-                'WHERE dt_recebimento <= now() '
-                'AND extract(year from dt_recebimento) = %s '
-                'group by concat(lpad(cast(extract(month from dt_recebimento) as text), 2, \'0\'), \'/\', extract(year from dt_recebimento)) '
-                'order by concat(lpad(cast(extract(month from dt_recebimento) as text), 2, \'0\'), \'/\', extract(year from dt_recebimento)) desc ')
+    consulta = consulta_mes_ano('aluguel_tbl', 'dt_recebimento', 'num_aluguel')
     cursor.execute(consulta, [ano])
     return cursor.fetchall()
 
 
 def recuperar_aluguel_ano():
     cursor = connection.cursor()
-    consulta = ('SELECT cast(extract(year from dt_recebimento) as bigint), sum(num_aluguel) '
-                'FROM aluguel_tbl '
-                'WHERE dt_recebimento <= now() '
-                'group by extract(year from dt_recebimento) '
-                'order by extract(year from dt_recebimento) desc ')
+    consulta = consulta_ano('aluguel_tbl', 'dt_recebimento', 'num_aluguel')
     cursor.execute(consulta)
     return cursor.fetchall()
 
 
 def recuperar_reforma_mes_ano(ano):
     cursor = connection.cursor()
-    consulta = ('SELECT concat(lpad(cast(extract(month from dt_reforma) as text), 2, \'0\'), \'/\', extract(year from dt_reforma)), sum(num_reforma) '
-                'FROM reforma_tbl '
-                'WHERE dt_reforma <= now() '
-                'AND extract(year from dt_reforma) = %s '
-                'group by concat(lpad(cast(extract(month from dt_reforma) as text), 2, \'0\'), \'/\', extract(year from dt_reforma)) '
-                'order by concat(lpad(cast(extract(month from dt_reforma) as text), 2, \'0\'), \'/\', extract(year from dt_reforma)) desc ')
+    consulta = consulta_mes_ano('reforma_tbl', 'dt_reforma', 'num_reforma')
     cursor.execute(consulta, [ano])
     return cursor.fetchall()
 
 
 def recuperar_reforma_ano():
     cursor = connection.cursor()
-    consulta = ('SELECT cast(extract(year from dt_reforma) as bigint), sum(num_reforma) '
-                'FROM reforma_tbl '
-                'WHERE dt_reforma <= now() '
-                'group by extract(year from dt_reforma) '
-                'order by extract(year from dt_reforma) desc ')
+    consulta = consulta_ano('reforma_tbl', 'dt_reforma', 'num_reforma')
     cursor.execute(consulta)
     return cursor.fetchall()
+
+
+def recuperar_administracao_mes_ano(ano):
+    cursor = connection.cursor()
+    consulta = consulta_mes_ano('aluguel_tbl', 'dt_recebimento', 'num_administracao')
+    cursor.execute(consulta, [ano])
+    return cursor.fetchall()
+
+
+def recuperar_administracao_ano():
+    cursor = connection.cursor()
+    consulta = consulta_ano('aluguel_tbl', 'dt_recebimento', 'num_administracao')
+    cursor.execute(consulta)
+    return cursor.fetchall()
+
+
+def consulta_mes_ano(tabela, coluna_data, coluna_valor):
+    return (
+        'SELECT concat(lpad(cast(extract(month from _coluna_data_) as text), 2, \'0\'), \'/\', extract(year from _coluna_data_)), sum(_coluna_valor_) '
+        'FROM _tabela_ '
+        'WHERE _coluna_data_ <= now() '
+        'AND extract(year from _coluna_data_) = %s '
+        'group by concat(lpad(cast(extract(month from _coluna_data_) as text), 2, \'0\'), \'/\', extract(year from _coluna_data_)) '
+        'order by concat(lpad(cast(extract(month from _coluna_data_) as text), 2, \'0\'), \'/\', extract(year from _coluna_data_)) desc '
+        .replace('_coluna_data_', coluna_data)
+        .replace('_coluna_valor_', coluna_valor)
+        .replace('_tabela_', tabela)
+    )
+
+
+def consulta_ano(tabela, coluna_data, coluna_valor):
+    return (
+        'SELECT cast(extract(year from _coluna_data_) as bigint), sum(_coluna_valor_) '
+        'FROM _tabela_ '
+        'WHERE _coluna_data_ <= now() '
+        'group by extract(year from _coluna_data_) '
+        'order by extract(year from _coluna_data_) desc '
+        .replace('_coluna_data_', coluna_data)
+        .replace('_coluna_valor_', coluna_valor)
+        .replace('_tabela_', tabela)
+    )
