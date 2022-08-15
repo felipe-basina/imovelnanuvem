a87b6df424b3df635e3cbdc8b04a4c9689ba586b
felipe-basina <felipe.basina@gmail.com>
2022-08-15 09:36:41 -0300

    Adicionado consultas para gerar relatorios: aluguel por ano, reforma por mes e ano, reforma por ano.

--- innapp/reports/consultas.py
+++ innapp/reports/consultas.py
@@ -13,3 +13,37 @@ def recuperar_aluguel_mes_ano(ano):
                 'order by concat(lpad(cast(extract(month from dt_recebimento) as text), 2, \'0\'), \'/\', extract(year from dt_recebimento)) desc ')
     cursor.execute(consulta, [ano])
     return cursor.fetchall()
+
+
+def recuperar_aluguel_ano():
+    cursor = connection.cursor()
+    consulta = ('SELECT cast(extract(year from dt_recebimento) as bigint), sum(num_aluguel) '
+                'FROM aluguel_tbl '
+                'WHERE dt_recebimento <= now() '
+                'group by extract(year from dt_recebimento) '
+                'order by extract(year from dt_recebimento) desc ')
+    cursor.execute(consulta)
+    return cursor.fetchall()
+
+
+def recuperar_reforma_mes_ano(ano):
+    cursor = connection.cursor()
+    consulta = ('SELECT concat(lpad(cast(extract(month from dt_reforma) as text), 2, \'0\'), \'/\', extract(year from dt_reforma)), sum(num_reforma) '
+                'FROM reforma_tbl '
+                'WHERE dt_reforma <= now() '
+                'AND extract(year from dt_reforma) = %s '
+                'group by concat(lpad(cast(extract(month from dt_reforma) as text), 2, \'0\'), \'/\', extract(year from dt_reforma)) '
+                'order by concat(lpad(cast(extract(month from dt_reforma) as text), 2, \'0\'), \'/\', extract(year from dt_reforma)) desc ')
+    cursor.execute(consulta, [ano])
+    return cursor.fetchall()
+
+
+def recuperar_reforma_ano():
+    cursor = connection.cursor()
+    consulta = ('SELECT cast(extract(year from dt_reforma) as bigint), sum(num_reforma) '
+                'FROM reforma_tbl '
+                'WHERE dt_reforma <= now() '
+                'group by extract(year from dt_reforma) '
+                'order by extract(year from dt_reforma) desc ')
+    cursor.execute(consulta)
+    return cursor.fetchall()
