8ab02d2e9a0df3b30432ab7d769ef072473afeb6
felipe-basina <felipe.basina@gmail.com>
2022-08-26 19:20:35 -0300

    Adicionado configuracoes para acesso a base de dados atraves de variaveis de ambiente.

--- inn/settings.py
+++ inn/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from urllib.parse import urlparse
 
 import dj_database_url
 import os
@@ -77,14 +78,19 @@ WSGI_APPLICATION = 'inn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASE_URL = os.getenv('DATABASE_URL', None)
+
+db_info = urlparse(DATABASE_URL)
+
 DATABASES = {
     'default': {
-        'ENGINE': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': db_info.username,
+        'PASSWORD': db_info.password,
+        'HOST': db_info.hostname,
+        'PORT': db_info.port,
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
